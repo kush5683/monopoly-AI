6ee0838ea37affc32f04e555120e2c52c2e14dda
Kush Shah <kush5683@gmail.com>
2022-06-13 17:42:10 -0400

    added more player functions

--- src/Player.py
+++ src/Player.py
@@ -1,16 +1,21 @@
 from typing import List
-
+from random import randint
 from src.property import Property
+from src.Space import Space
 
 
 class Player:
-    def __init__(self, player_id, balance, properties):
+    def __init__(self, player_id: int, balance: float, properties: List[Property], current_space: Space):
         self.id = player_id
         self.balance = balance
         self.properties = properties
+        self.current_space = current_space
 
     def get_id(self) -> int:
         return self.id
+
+    def get_current_space(self) -> Space:
+        return self.current_space
 
     def get_balance(self) -> float:
         return self.balance
@@ -33,4 +38,7 @@ class Player:
     def remove_balance(self, amount: float) -> float:
         self.balance -= amount
         return self.balance
+
+    def roll_dice(self) -> int:
+        return randint(1, 6)
 
